fe4576f81708b3f886c406155ffece12f41c057e
ericcarlotto <ericitapiruba@hotmail.com.br>
2022-06-13 03:24:08 -0300

    Created a test with chromedriver

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -1,20 +1,3 @@
-/*
- * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package io.github.bonigarcia.wdm.test;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
@@ -35,7 +18,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 /**
  * Test with Chrome.
  *
- * @author Boni Garcia (boni.gg@gmail.com)
+ * @author Eric Carlotto (eric.carlottoitapiruba@gmail.com)
  * @since 1.0.0
  */
 public class ChromeTest {
@@ -60,19 +43,17 @@ public class ChromeTest {
     }
 
     @Test
+    // Test done by Eric Carlotto, for product quality class study purposes.
     public void test() {
-        // Your test code here. For example:
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        By searchInput = By.id("searchInput");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
-        wait.until(elementToBeClickable(searchButton));
-        driver.findElement(searchButton).click();
+        driver.get("https://financeone.com.br/conversor-de-moedas/");
 
+        By searchInput = By.id("currency-value");
+        wait.until(presenceOfElementLocated(searchInput));
+
+        driver.findElement(searchInput).sendKeys("2300");
         wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+                "50.008,86 BRL"));
     }
 
 }
